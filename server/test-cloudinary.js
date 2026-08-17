e64264a0a2f4ead9644bfd990579d2b114aa4b8b
olaleye <olaleyesamuel426@gmail.com>
2026-08-17 10:38:37 +0100

    cim

--- server/test-cloudinary.js
+++ server/test-cloudinary.js
@@ -3,8 +3,8 @@ import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: '432241938564531',
+  api_secret: 'sRiRb6jlI5kZJTpcmICmovwSshc'
 });
 
 console.log('Config:', cloudinary.config());
